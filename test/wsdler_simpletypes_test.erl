cf1eaffcb55d7fdec5d092fc5f84bdca665c2d27
Erik Søe Sørensen <ess@trifork.com>
2013-08-29 19:11:59 +0200

    Add simple type generation test.
    (It fails, at present.)

---
+++ test/wsdler_simpletypes_test.erl
@@ -0,0 +1,33 @@
+-module(wsdler_simpletypes_test).
+
+-include("../src/wsdler.hrl").
+
+-include_lib("eunit/include/eunit.hrl").
+-undef(LET).
+-include_lib("triq/include/triq.hrl").
+
+prop_boolean_type() ->
+    ?FORALL(X, simple_type_generator("xsd:boolean"),
+            lists:member(X, ["0","1","false","true"])).
+
+simple_type_generator(BaseType) ->
+    SchemaSrc = schema(BaseType),
+    {ok,TypeDict} = wsdler_wsdl:parse_xsd(SchemaSrc),
+    Type = dict:fetch(qtypename("T"), TypeDict),
+    wsdler_generators:generator(Type).
+
+schema(BaseType) ->
+    "<xsd:schema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\""
+        " targetNamespace=\"http://www.example.org\""
+        " xmlns=\"http://www.example.org\""
+        " elementFormDefault=\"qualified\">"
+        "<xsd:simpleType name=\"T\"><xsd:restriction base=\""++BaseType++"\"/></xsd:simpleType>"
+        "</xsd:schema>".
+
+qtypename(LocalName) ->
+     {"http://www.example.org", LocalName}.
+
+property_test_() ->
+    {timeout, 60,
+     fun() -> ?assert(triq:module(?MODULE)) end
+    }.
