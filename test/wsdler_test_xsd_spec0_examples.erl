5ac8b054f24446093930ee56145dfd54171d98e6
Erik Søe Sørensen <ess@trifork.com>
2013-09-05 00:13:25 +0200

    XSD spec-examples test: Give each test a eunit name. Also, the name 'gen_test' confused eunit.

--- test/wsdler_test_xsd_spec0_examples.erl
+++ test/wsdler_test_xsd_spec0_examples.erl
@@ -7,15 +7,22 @@
 xsd_spec0_examples_test_() ->
     FileName = code:lib_dir(wsdler,test)++"/test-xsd.xml",
     {ok,Text} = file:read_file(FileName),
-    foreach(Text, fun gen_test/1, []).
+    Schemas = split_schemas(Text),
+    {foreach, fun()->ok end,
+     [{lists:flatten(io_lib:format("Schema #~p", [Nr])), % title
+       fun() -> test_schema(Schema) end}
+      || {Nr, Schema} <- Schemas]}.
+    %% foreach(Text, fun gen_test/1, []).
 
-gen_test(XML) ->
+test_schema(XML) ->
     fun() -> ?assertMatch([_], wsdler_xsd:do_schema(XML)) end.
 
-foreach("\n"++Rest, Fun, Acc) ->
-    foreach(Rest, Fun, Acc);
-foreach([], _Fun, Acc) ->
+split_schemas(Text) ->
+    split_schemas(Text, 0, []).
+split_schemas("\n"++Rest, Nr, Acc) ->
+    split_schemas(Rest, Nr, Acc);
+split_schemas([], _Nr, Acc) ->
     lists:reverse(Acc);
-foreach(Text, Fun, Acc) ->
+split_schemas(Text, Nr, Acc) ->
     {ok,XML,Rest} = erlsom:simple_form(Text, wsdler_xml:erlsom_options()),
-    foreach(Rest, Fun, [Fun(XML)|Acc]).
+    split_schemas(Rest, Nr+1, [{Nr+1,XML} | Acc]).
