5686f916c28b467217770d47b709a91cd0af50c7
Erik Søe Sørensen <ess@trifork.com>
2013-09-19 16:21:20 +0200

    Split test-xsd.xml into one file per schema.

--- test/wsdler_xsdfile_tests.erl
+++ test/wsdler_xsdfile_tests.erl
@@ -3,24 +3,17 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-xsdfiles_test() ->
+xsdfiles_test_() ->
     TestDataDir = filename:join(filename:join(code:lib_dir(wsdler), "test"), "data"),
     {ok, DataFiles0} = file:list_dir(TestDataDir),
     DataFiles = [filename:join(TestDataDir,X) || X <- DataFiles0, lists:suffix(".xsd",X)],
-    [x = (catch one_xsd_file(X)) || X <- DataFiles].
+    [{"Schema file "++X, fun()->one_xsd_file(X) end} || X <- DataFiles].
 
 one_xsd_file(Filename) ->
-    {ok,XMLFragment} = file:read_file(Filename),
-    SchemaXML =
-        ["<xsd:schema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\""
-        " targetNamespace=\"http://www.example.org\""
-        " xmlns=\"http://www.example.org\""
-        " elementFormDefault=\"qualified\">",
-         XMLFragment,
-        "</xsd:schema>"],
-    {ok, TypeMap} = wsdler_wsdl:parse_xsd(unicode:characters_to_list(SchemaXML)),
-    TypeNames = [TN || {{_,TN}, _} <- dict:to_list(TypeMap)],
-    x = TypeNames.
+    {ok, Schema} = wsdler_xsd:parse_file(Filename),
+    TypeNames = [TN || {{_,TN}, _} <- wsdler_xsd:schema_to_type_list(Schema)],
+    %% TODO: Generate samples for each type...
+    TypeNames.
 
 
 
