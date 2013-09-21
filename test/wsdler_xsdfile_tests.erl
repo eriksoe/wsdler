520f2428fd40d8e634dc01f4ef9b788ab5408d51
Erik Søe Sørensen <ess@trifork.com>
2013-09-21 10:36:06 +0200

    xsdfiles_test_: Better error reporting w/ eunit.

--- test/wsdler_xsdfile_tests.erl
+++ test/wsdler_xsdfile_tests.erl
@@ -7,7 +7,7 @@ xsdfiles_test_() ->
     TestDataDir = filename:join(filename:join(code:lib_dir(wsdler), "test"), "data"),
     {ok, DataFiles0} = file:list_dir(TestDataDir),
     DataFiles = [filename:join(TestDataDir,X) || X <- DataFiles0, lists:suffix(".xsd",X)],
-    [{"Schema file "++X, fun()->one_xsd_file(X) end} || X <- DataFiles].
+    [{"Schema file "++X, fun()->{ok,_}=(catch {ok,one_xsd_file(X)}) end} || X <- DataFiles].
 
 one_xsd_file(Filename) ->
     {ok, Schema} = wsdler_xsd:parse_file(Filename),
